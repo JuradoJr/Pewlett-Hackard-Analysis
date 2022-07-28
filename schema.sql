9be3099cbab28a441dd47e22907dddd327e86be5
JuradoJr <104862099+JuradoJr@users.noreply.github.com>
2022-07-27 17:56:35 -0700

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -19,8 +19,8 @@ CREATE TABLE employees (
 );
 
 --Dept_Manager Table
-CREATE TABLE debt_manager (
-    dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE titles (
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, title, from_date)
 );
 
 -- Dept_Empt Table
@@ -61,3 +61,43 @@ PRIMARY KEY (dept_no, emp_no)
 );
 
 SELECT * FROM departments; 
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01'  AND '1955-12-31';
+
+SELECT first_name,last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
